3abad73134172957abc87b8d2065ea94b1ace3ce
Pradhyum R <drpradhyum2016@outlook.com>
2022-04-18 01:00:40 +0000

    combo

---
+++ combo/combo.py
@@ -0,0 +1,17 @@
+"""
+ID: drpradh1
+LANG: PYTHON3
+TASK: combo
+"""
+
+import itertools as it
+
+with open('combo.in') as f:
+    n = int(f.readline())
+    c1 = map(int, f.readline().split())
+    c2 = map(int, f.readline().split())
+
+def sum_tuples(t1, t2):
+    return 
+
+it.product([-2, -1, 0, 1, 2], repeat=3)
